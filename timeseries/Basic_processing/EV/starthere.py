1d3b85dd6c40b181ee752c60f3946a7b591ffbad
Jussi Ikaheimo <jussi.ikaheimo@vtt.fi>
2023-12-01 10:26:45 +0200

    in Basic_processing/EV moving the entry point to a separate file.

---
+++ timeseries/Basic_processing/EV/starthere.py
@@ -0,0 +1,9 @@
+
+from create_ev_time_series import create_ev_timeseries
+
+
+# scenario and year of EV capacity, e.g. "Distributed Energy", 2030
+scenario = "Distributed Energy"
+year = 2040
+
+create_ev_timeseries(scenario, year)
