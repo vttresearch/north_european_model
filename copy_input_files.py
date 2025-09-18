6536b428e347353d57d61f06c479a8558ebae067
eriharriso <eriharriso@protonmail.com>
2025-09-18 14:10:35 +0300

    Prevented inputData file copying from ts_data_path

--- copy_input_files.py
+++ copy_input_files.py
@@ -11,10 +11,11 @@ def main():
         folder_names = source_db.scenario(name=scenario_name)["alternative_name_list"]
     shutil.copy(gdx_file, output_folder)
     for folder_name in folder_names:
-        if ne_model_path.exists():
-            copy_files(ne_model_path / folder_name)
+        ts_data_path = ne_model_path / folder_name
+        if ts_data_path.exists():
+            copy_files(ts_data_path)
         else:
-            print(f"Warning: {input_folder} does not exist.")
+            print(f"Warning: {ts_data_path} does not exist.")
     invest_schedule_path = pathlib.Path("input_invest_and_schedule") # target
     invest_schedule_path.mkdir(exist_ok=True)
     for file_path in input_invest_schedule.iterdir():
@@ -24,7 +25,7 @@ def main():
 def copy_files(input_folder):
     for filename in os.listdir(input_folder):
         print(f"Copying {filename} from {input_folder} to {output_folder}")
-        if filename.endswith(".csv") or filename.endswith(".gdx") or filename.endswith(".inc") or filename.endswith(".gms"):
+        if any(filename.endswith(ext) for ext in [".csv", ".gdx", ".inc", ".gms"]) and not filename.startswith("inputData"):
             file_path = os.path.join(input_folder, filename)
             if os.path.isfile(file_path):
                 shutil.copy(file_path, output_folder)
